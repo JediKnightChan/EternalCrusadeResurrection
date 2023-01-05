490bfbdd5cb095cc568bb629517174bf9f0ab848
JediKnight <jediknightchannel@yandex.ru>
2023-01-05 16:49:09 +0300

    Add character customization from Lyra

---
+++ Source/ECR/Public/Cosmetics/ECRCharacterPartTypes.h
@@ -0,0 +1,77 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "ECRCharacterPartTypes.generated.h"
+
+class UChildActorComponent;
+class UECRPawnComponent_CharacterParts;
+struct FECRCharacterPartList;
+
+//////////////////////////////////////////////////////////////////////
+
+// How should collision be configured on the spawned part actor
+UENUM()
+enum class ECharacterCustomizationCollisionMode : uint8
+{
+	// Disable collision on spawned character parts
+	NoCollision,
+
+	// Leave the collision settings on character parts alone
+	UseCollisionFromCharacterPart
+};
+
+//////////////////////////////////////////////////////////////////////
+
+// A handle created by adding a character part entry, can be used to remove it later
+USTRUCT(BlueprintType)
+struct FECRCharacterPartHandle
+{
+	GENERATED_BODY()
+
+	void Reset()
+	{
+		PartHandle = INDEX_NONE;
+	}
+
+	bool IsValid() const
+	{
+		return PartHandle != INDEX_NONE;
+	}
+
+private:
+	UPROPERTY()
+	int32 PartHandle = INDEX_NONE;
+
+	friend FECRCharacterPartList;
+};
+
+//////////////////////////////////////////////////////////////////////
+// A character part request
+
+USTRUCT(BlueprintType)
+struct FECRCharacterPart
+{
+	GENERATED_BODY()
+
+	// The part to spawn
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> PartClass;
+
+	// The socket to attach the part to (if any)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SocketName;
+
+	// How to handle collision for the primitive components in the part
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ECharacterCustomizationCollisionMode CollisionMode = ECharacterCustomizationCollisionMode::NoCollision;
+
+	// Compares against another part, ignoring the collision mode
+	static bool AreEquivalentParts(const FECRCharacterPart& A, const FECRCharacterPart& B)
+	{
+		return (A.PartClass == B.PartClass) && (A.SocketName == B.SocketName);
+	}
+};
---
+++ Source/ECR/Public/Cosmetics/ECRControllerComponent_CharacterParts.cpp
@@ -0,0 +1,125 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ECRControllerComponent_CharacterParts.h"
+#include "ECRPawnComponent_CharacterParts.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/CheatManager.h"
+
+//////////////////////////////////////////////////////////////////////
+
+UECRControllerComponent_CharacterParts::UECRControllerComponent_CharacterParts(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UECRControllerComponent_CharacterParts::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Listen for pawn possession changed events
+	if (HasAuthority())
+	{
+		if (AController* OwningController = GetController<AController>())
+		{
+			OwningController->OnPossessedPawnChanged.AddDynamic(this, &ThisClass::OnPossessedPawnChanged);
+
+			if (APawn* ControlledPawn = GetPawn<APawn>())
+			{
+				OnPossessedPawnChanged(nullptr, ControlledPawn);
+			}
+		}
+	}
+}
+
+void UECRControllerComponent_CharacterParts::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	RemoveAllCharacterParts();
+	Super::EndPlay(EndPlayReason);
+}
+
+UECRPawnComponent_CharacterParts* UECRControllerComponent_CharacterParts::GetPawnCustomizer() const
+{
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		return ControlledPawn->FindComponentByClass<UECRPawnComponent_CharacterParts>();
+	}
+	return nullptr;
+}
+
+void UECRControllerComponent_CharacterParts::AddCharacterPart(const FECRCharacterPart& NewPart)
+{
+	AddCharacterPartInternal(NewPart, ECharacterPartSource::Natural);
+}
+
+void UECRControllerComponent_CharacterParts::AddCharacterPartInternal(const FECRCharacterPart& NewPart, ECharacterPartSource Source)
+{
+	FECRControllerCharacterPartEntry& NewEntry = CharacterParts.AddDefaulted_GetRef();
+	NewEntry.Part = NewPart;
+	NewEntry.Source = Source;
+
+	if (UECRPawnComponent_CharacterParts* PawnCustomizer = GetPawnCustomizer())
+	{
+		if (NewEntry.Source != ECharacterPartSource::NaturalSuppressedViaCheat)
+		{
+			NewEntry.Handle = PawnCustomizer->AddCharacterPart(NewPart);
+		}
+	}
+
+}
+
+void UECRControllerComponent_CharacterParts::RemoveCharacterPart(const FECRCharacterPart& PartToRemove)
+{
+	for (auto EntryIt = CharacterParts.CreateIterator(); EntryIt; ++EntryIt)
+	{
+		if (FECRCharacterPart::AreEquivalentParts(EntryIt->Part, PartToRemove))
+		{
+			if (UECRPawnComponent_CharacterParts* PawnCustomizer = GetPawnCustomizer())
+			{
+				PawnCustomizer->RemoveCharacterPart(EntryIt->Handle);
+			}
+
+			EntryIt.RemoveCurrent();
+			break;
+		}
+	}
+}
+
+void UECRControllerComponent_CharacterParts::RemoveAllCharacterParts()
+{
+	if (UECRPawnComponent_CharacterParts* PawnCustomizer = GetPawnCustomizer())
+	{
+		for (FECRControllerCharacterPartEntry& Entry : CharacterParts)
+		{
+			PawnCustomizer->RemoveCharacterPart(Entry.Handle);
+		}
+	}
+
+	CharacterParts.Reset();
+}
+
+void UECRControllerComponent_CharacterParts::OnPossessedPawnChanged(APawn* OldPawn, APawn* NewPawn)
+{
+	// Remove from the old pawn
+	if (UECRPawnComponent_CharacterParts* OldCustomizer = OldPawn ? OldPawn->FindComponentByClass<UECRPawnComponent_CharacterParts>() : nullptr)
+	{
+		for (FECRControllerCharacterPartEntry& Entry : CharacterParts)
+		{
+			OldCustomizer->RemoveCharacterPart(Entry.Handle);
+			Entry.Handle.Reset();
+		}
+	}
+
+	// Apply to the new pawn
+	if (UECRPawnComponent_CharacterParts* NewCustomizer = NewPawn ? NewPawn->FindComponentByClass<UECRPawnComponent_CharacterParts>() : nullptr)
+	{
+		for (FECRControllerCharacterPartEntry& Entry : CharacterParts)
+		{
+			ensure(!Entry.Handle.IsValid());
+
+			if (Entry.Source != ECharacterPartSource::NaturalSuppressedViaCheat)
+			{
+				Entry.Handle = NewCustomizer->AddCharacterPart(Entry.Part);
+			}
+		}
+	}
+}
