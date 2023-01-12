30c5976fd7e0db2138fd9a17bc1ec422b8454bd4
JediKnight <jediknightchannel@yandex.ru>
2023-01-13 02:38:10 +0300

    Add static camera mode

---
+++ Source/ECR/Private/Gameplay/Camera/ECRCameraMode_Static.cpp
@@ -0,0 +1,38 @@
+﻿#include "Gameplay/Camera/ECRCameraMode_Static.h"
+#include "Curves/CurveVector.h"
+
+
+void UECRCameraMode_Static::OnActivation()
+{
+	Super::OnActivation();
+	TimeRunning = 0.0f;
+	SavedControlRotation = GetPivotRotation();
+}
+
+void UECRCameraMode_Static::UpdateView(float DeltaTime)
+{
+	TimeRunning += DeltaTime;
+
+	// Constructing pivot transform
+	const FVector ActorLocation = GetPivotLocation();
+
+	const AActor* TargetActor = GetTargetActor();
+	check(TargetActor);
+	const FRotator ActorRotation = TargetActor->GetActorRotation();
+	const FTransform ActorTransform{{0, ActorRotation.Yaw, 0}, ActorLocation, {1, 1, 1}};
+
+	UE_LOG(LogTemp, Warning, TEXT("Global pivot %s"), *(ActorTransform.ToString()));
+	
+	// Constructing local camera transform from curves
+	const FVector CameraLocalLocation = TimeLocationCurve->GetVectorValue(TimeRunning);
+	const FVector CurveRotatorValue = TimeRotationCurve->GetVectorValue(TimeRunning);
+	const FRotator CameraLocalRotation = FRotator{CurveRotatorValue.X, CurveRotatorValue.Y, CurveRotatorValue.Z};
+	const FTransform CameraLocalTransform{CameraLocalRotation, CameraLocalLocation, {1, 1, 1}};
+
+	UE_LOG(LogTemp, Warning, TEXT("Local camera %s"), *(CameraLocalTransform.ToString()));
+	const FTransform CameraGlobalTransform = CameraLocalTransform * ActorTransform;
+	View.Location = CameraGlobalTransform.GetLocation();
+	View.Rotation = CameraGlobalTransform.Rotator();
+	View.ControlRotation = SavedControlRotation;
+	View.FieldOfView = FieldOfView;
+}
---
+++ Source/ECR/Public/Gameplay/Camera/ECRCameraMode_Static.h
@@ -0,0 +1,40 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ECRCameraMode.h"
+#include "Curves/CurveFloat.h"
+
+#include "ECRCameraMode_Static.generated.h"
+
+class UCurveVector;
+
+/**
+ * UECRCameraMode_Static
+ *
+ *	A basic camera mode for camera not depending on user input, but on time since activation
+ */
+UCLASS(Abstract, Blueprintable)
+class UECRCameraMode_Static : public UECRCameraMode
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnActivation() override;
+
+protected:
+	virtual void UpdateView(float DeltaTime) override;
+
+protected:
+	float TimeRunning;
+	FRotator SavedControlRotation;
+
+	// Curve that defines location of camera using the time since camera mode activation to evaluate the curve.
+	UPROPERTY(EditDefaultsOnly, Category = "Third Person")
+	const UCurveVector* TimeLocationCurve;
+
+	// Curve that defines rotation of camera using the time since camera mode activation to evaluate the curve.
+	UPROPERTY(EditDefaultsOnly, Category = "Third Person")
+	const UCurveVector* TimeRotationCurve;
+};
