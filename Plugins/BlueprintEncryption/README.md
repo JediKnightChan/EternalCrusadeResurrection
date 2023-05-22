63a665c39e0f98185eaba338b49739a7d931a32e
JediKnight <jediknightchannel@yandex.ru>
2023-05-22 16:05:40 +0300

    Encryption plugin from Aaron Hunt

---
+++ Plugins/BlueprintEncryption/README.md
@@ -0,0 +1,4 @@
+﻿# Blueprint encryption plugin
+
+Original author is Aaron Hunt's. 
+Original [repository](https://github.com/anhuntz/blueprint-encryption).
---
+++ Plugins/BlueprintEncryption/Source/BlueprintEncryption/Public/BlueprintEncryption.h
@@ -0,0 +1,14 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FBlueprintEncryptionModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
