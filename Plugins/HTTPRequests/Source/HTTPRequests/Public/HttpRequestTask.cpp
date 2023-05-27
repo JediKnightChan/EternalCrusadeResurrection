4b6e2b17d57762ccf49b3108628e220a26ae6dc8
JediKnightChan <jediknightchannel@yandex.ru>
2023-05-27 03:17:48 +0300

    Http Requests plugin

---
+++ Plugins/HTTPRequests/Source/HTTPRequests/Public/HttpRequestTask.cpp
@@ -0,0 +1,47 @@
+﻿#include "HttpRequestTask.h"
+
+
+#include "Interfaces/IHttpResponse.h"
+
+
+void UHttpRequestAsyncAction::Activate()
+{
+	Super::Activate();
+
+	FHttpModule* Module = &FHttpModule::Get();
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Module->CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &ThisClass::OnPostReceivedResponse);
+
+	Request->SetURL(URL);
+	Request->SetVerb(Method);
+	for (auto [HeaderKey, HeaderValue] : Headers)
+	{
+		Request->SetHeader(HeaderKey, HeaderValue);
+	}
+	Request->SetContentAsString(Content);
+	Request->ProcessRequest();
+}
+
+UHttpRequestAsyncAction* UHttpRequestAsyncAction::HttpRequestAsyncAction(
+	FString Method, FString Url, TMap<FString, FString> Headers, FString Content)
+{
+	UHttpRequestAsyncAction* MyAction = NewObject<UHttpRequestAsyncAction>();
+	MyAction->Method = Method;
+	MyAction->URL = Url;
+	MyAction->Headers = Headers;
+	MyAction->Content = Content;
+	return MyAction;
+}
+
+void UHttpRequestAsyncAction::OnPostReceivedResponse(FHttpRequestPtr Request, FHttpResponsePtr Response,
+                                                     bool bWasSuccessful)
+{
+	if (bWasSuccessful && Response.Get())
+	{
+		OnSuccess.Broadcast(Response->GetResponseCode(), Response->GetContentAsString());
+	}
+	else
+	{
+		OnFailed.Broadcast();
+	}
+}
