10bb3cb72b74ef7cf6e6c5cd03475662e0c59e8d
JediKnightChan <jediknightchannel@yandex.ru>
2024-10-05 15:46:05 +0300

    Helper library to convert string to gameplay tag in Python

---
+++ Source/ECREditorModifications/Private/ECRPythonHelpersLibrary.cpp
@@ -0,0 +1,15 @@
+﻿#include "ECRPythonHelpersLibrary.h"
+
+FGameplayTag UECRPythonHelpersLibrary::ConvertStringToGameplayTag(const FString& TagName)
+{
+	// Get the GameplayTagsManager instance
+	FGameplayTag Tag = FGameplayTag::RequestGameplayTag(FName(*TagName));
+
+	// Optional: Check if the tag exists
+	if (!Tag.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Invalid GameplayTag: %s"), *TagName);
+	}
+
+	return Tag;
+}
---
+++ Source/ECREditorModifications/Public/ECRPythonHelpersLibrary.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameplayTagContainer.h"
+
+#include "ECRPythonHelpersLibrary.generated.h"
+
+UCLASS()
+class ECREDITORMODIFICATIONS_API UECRPythonHelpersLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "GameplayTags", meta = (ScriptMethod))
+	static FGameplayTag ConvertStringToGameplayTag(const FString& TagName);
+};
